d73d2c0c3b31ddc40799776d6e11546d8be2661c
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-02-13 19:24:40 +0200

    [feat] Add Netlify config

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GlobalStyle } from './Global.styled.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <GlobalStyle />
       <App />
     </BrowserRouter>
